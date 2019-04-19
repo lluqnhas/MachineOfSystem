bf3f94a95ead13127871c286393d233fce970018
lluqn <lluqnhas@gmal.com>
2019-04-19 02:53:57 -0300

    piloto

--- app/src/main/java/com/example/lluqn/machineofsystem/MainActivity.java
+++ app/src/main/java/com/example/lluqn/machineofsystem/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.lluqn.machineofsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText login, senha;
+    Button bt_entrar;
+    TextView add_membro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        senha = findViewById(R.id.login);
+
+        bt_entrar = findViewById(R.id.bt_entrar);
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //evento do click
+                    }
+                });
+
+        add_membro = findViewById(R.id.add_membro);
+        add_membro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //evento do click
+            }
+        });
     }
 }
